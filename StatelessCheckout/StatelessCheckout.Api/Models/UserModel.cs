00b9df0ff7b327deb7081da4cc4452f955c0fc44
Daniel Vo <dvo@midwayusa.com>
2016-06-22 22:45:06 -0500

    Pending changes for tokenization

---
+++ StatelessCheckout/StatelessCheckout.Api/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StatelessCheckout.Api.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
